7c385ac5cf2dfef90a38ddd05bed5c44f1b28cb6
Oxcorp <braving@hotmail.com>
2017-03-24 02:05:08 -0400

    Fibonacci sequence

--- fib.py
+++ fib.py
@@ -1,20 +1,20 @@
-def fibonnaci(n):
+def fibonacci(n):
 	if n == 0:
 		return 0
 	elif n == 1:
 		return 1
 	else:
-		return fibonnaci(n-1) + fibonnaci(n-2)
+		return fibonacci(n-1) + fibonacci(n-2)
 
 
 #Program loop
 while True:
 	try:
-		print "We will calculate the result of the fibonnaci sequence given the input."
+		print "We will calculate the result of the fibonacci sequence given the input."
 		input_int = int(raw_input("Input: "))
 		temp = []
 		for x in range(input_int):
-			temp.append(fibonnaci(x))
+			temp.append(fibonacci(x))
 		print "Result:", temp, "\n"
 	except:
 		print "Input error."
